0adaf782f8c42b8987abc34e00bced6c10330ca6
GEANYL PAUL ALOJADO NILLOS <2200691@cc.usls.edu>
2025-02-13 11:25:18 +0800

    Feat: Creating item add form.

--- app/Http/Controllers/HelloWorld.php
+++ app/Http/Controllers/HelloWorld.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class HelloWorld extends Controller
 {
-    //
+    public function index()
+    {
+        return view('HelloWorld');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\HelloWorld;
+use App\Http\Controllers\Create;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/Hello', function () {
-    return view('HelloWorld');
-});
-
-
+Route::get('/Hello', [HelloWorld::class, 'index']);
+Route::get('/create', [Create::class, 'create']);
