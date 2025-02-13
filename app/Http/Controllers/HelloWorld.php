a10a83a20147570e40594c182255e8aa3e18258b
GEANYL PAUL ALOJADO NILLOS <2200691@cc.usls.edu>
2025-02-13 11:00:14 +0800

    hello world

---
+++ app/Http/Controllers/HelloWorld.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HelloWorld extends Controller
+{
+    //
+}
--- resources/views/HelloWorld.blade.php
+++ resources/views/HelloWorld.blade.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Document</title>
 </head>
-    <body>
-        <h1> test </h1>
-    </body>
+<body>
+    <h1>Hello World asdas</h1>
+</body>
 </html>
