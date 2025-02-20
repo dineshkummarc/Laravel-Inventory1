0f8f6a6d81ecdc091fc45ac8191a9ec2ff01df30
GEANYL PAUL ALOJADO NILLOS <2200691@cc.usls.edu>
2025-02-20 10:47:17 +0800

    Feat: Seeding data into database,

--- database/seeders/ItemsTableSeeder.php
+++ database/seeders/ItemsTableSeeder.php
@@ -11,11 +11,10 @@ class ItemsTableSeeder extends Seeder
     {
         DB::table('items')->insert([
             ['name' => 'Laptop', 'price' => 599.99, 'category_id' => 1], // Technology
-            ['name' => 'Smartphone', 'price' => 799.99, 'category_id' => 1], // Technology
-            ['name' => 'Yoga Mat', 'price' => 25.00, 'category_id' => 2], // Health
-            ['name' => 'Vitamins', 'price' => 15.00, 'category_id' => 2], // Health
-            ['name' => 'Math Textbook', 'price' => 39.99, 'category_id' => 3], // Education
-            ['name' => 'Notebook', 'price' => 5.00, 'category_id' => 3], // Education
+            ['name' => 'Food', 'price' => 799.99, 'category_id' => 2], // Health
+            ['name' => 'books', 'price' => 25.00, 'category_id' => 3], // Education
+            ['name' => 'test 1', 'price' => 15.00, 'category_id' => 4], // test 1
+            ['name' => 'test 2', 'price' => 39.99, 'category_id' => 5], //  test 2
         ]);
     }
 }
