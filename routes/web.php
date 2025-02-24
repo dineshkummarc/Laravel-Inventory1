fafccb1aa0087cda8269110810754cb40154b1fc
Geanyl17 <134925077+Geanyl17@users.noreply.github.com>
2025-02-24 23:22:43 +0800

    Feat: Making page responsive and Integrating Bootstrap

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\InventoryController;
+use App\Http\Controllers\Login;
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HelloWorld;
 use App\Http\Controllers\Create;
@@ -8,3 +11,7 @@ Route::get('/', function () {
 });
 Route::get('/Hello', [HelloWorld::class, 'index']);
 Route::get('/create', [Create::class, 'create']);
+Route::get('/login', [LoginController::class, 'login']);
+Route::post('/login', [LoginController::class, 'auth']);
+Route::get('/Inventory', [InventoryController::class, 'Inv']);
+
