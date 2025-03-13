2b6f518884ad892bbcad8dbb3872535893bad69a
GEANYL PAUL ALOJADO NILLOS <2200691@cc.usls.edu>
2025-03-13 11:18:18 +0800

    Displaying categories data with using laravel directives

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -10,13 +10,19 @@
       <tr>
         <th>ID</th>
         <th>Category Name</th>
+        <th>Description</th>
+        <th>Created At</th>
+        <th>Updated At</th>
       </tr>
     </thead>
     <tbody>
       @foreach($categories as $category)
         <tr>
           <td>{{ $category->id }}</td>
-          <td>{{ $category->category_name }}</td>
+          <td>{{ $category->name }}</td>
+          <td>{{ $category->description ?? 'No description' }}</td>
+          <td>{{ $category->created_at ? $category->created_at->format('Y-m-d H:i') : 'N/A' }}</td>
+          <td>{{ $category->updated_at ? $category->updated_at->format('Y-m-d H:i') : 'N/A' }}</td>
         </tr>
       @endforeach
     </tbody>
